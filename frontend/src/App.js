573895edf38689154b79cdadc7379ce18fa6cd83
Goddess-Icon <reecesablanftw@gmail.com>
2024-02-25 13:57:15 -0500

    Reece testing minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
     <main className ="py-3">
       <Container>
     <h1>Welcome to JukeBoxd</h1>
+    <p>JukeBoxd is currently a work in progress by CEN capstone students</p>
         </Container>
       </main>
     </>
