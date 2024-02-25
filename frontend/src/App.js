4d357f5766770d89439cb32f0d0f7be29d82b10b
Goddess-Icon <reecesablanftw@gmail.com>
2024-02-25 15:50:46 -0500

    rating buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,36 @@ import Header from './components/Header';
 import { Container } from 'react-bootstrap';
 import './App.css';  // Import your CSS file
 
+const StarRating = ({ rating, onStarClick }) => {
+  const stars = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="star-rating">
+      {stars.map((star) => (
+        <button
+          key={star}
+          className={star <= rating ? "active" : ""}
+          onClick={() => onStarClick(star)}
+        >
+          &#9733; {/* Unicode character for a star */}
+        </button>
+      ))}
+    </div>
+  );
+};
+
 const App = () => {
   const [buttonText, setButtonText] = useState('Do you like this album?');
+  const [rating, setRating] = useState(0);
+  const handleStarClick = (selectedRating) => {
+    setRating(selectedRating);
+  };
 
   const handleButtonClick = () => {
     setButtonText('Swifty spotted');
     
   };
+  
   return (
     <>
     <Header />
@@ -24,6 +47,8 @@ const App = () => {
     <button className="interactiveButton" onClick={handleButtonClick}>
             {buttonText}
           </button>
+          <StarRating rating={rating} onStarClick={handleStarClick} />
+          <p>Your Rating: {rating} stars</p>
         </Container>
       </main>
     </>
