3e57517db8365208f1a6a679669b31e033015e61
Goddess-Icon <reecesablanftw@gmail.com>
2024-02-25 15:42:21 -0500

    added button and changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
 import './App.css';  // Import your CSS file
 
 const App = () => {
+  const [buttonText, setButtonText] = useState('Do you like this album?');
+
+  const handleButtonClick = () => {
+    setButtonText('Swifty spotted');
+    
+  };
   return (
     <>
     <Header />
@@ -12,7 +18,12 @@ const App = () => {
     <h1>Welcome to JukeBoxd</h1>
     <p className="bodyText">JukeBoxd is currently a work in progress created by CEN capstone students</p>
 
-    <img src="Images/tswift album.jpeg" alt ="tswift" width="200" height="200"/>
+    
+    <img src="Images/tswift album.jpeg" alt ="tswift" width="300" height="300"/>
+    
+    <button className="interactiveButton" onClick={handleButtonClick}>
+            {buttonText}
+          </button>
         </Container>
       </main>
     </>
