a87ba5811e4a0d82d4616feb270050859cc90391
krennster <krennster17@gmail.com>
2024-04-18 10:56:53 -0400

    krenns changes

---
+++ backend/data/blogs.js
@@ -0,0 +1,43 @@
+const blogs = [
+    {
+        username: 'doughboy420',
+        subject: 'Is Taylor Swift the Greatest to Ever Do It?',
+        content: 'Blah blah blah blah blah blah blah blah blah blahblah blah blah blah blahblah blah blah blah blah blah blah blahblahblahblahblahblahblahblah blahblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+        tag: 'Talor Swift',
+    },
+
+    {
+        username: 'admin',
+        subject: 'Who is Better: NBA Youngboy or Youngboy Never Broke Again?',
+        content: 'Blah blah blah blah blah blah blah blah blah blahblah blah blah blah blahblah blah blah blah blah blah blah blahblahblahblahblahblahblahblah blahblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+        tag: 'Kentrell',
+    },
+
+
+    {
+        username: 'admin',
+        subject: 'What are some of your favorite OSTs from your favorite games?',
+        content: 'Blah blah blah blah blah blah blah blah blah blahblah blah blah blah blahblah blah blah blah blah blah blah blahblahblahblahblahblahblahblah blahblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+        tag: 'Video Games',
+    },
+
+
+    {
+        username: 'janeFromStateFarm',
+        subject: 'Who would win in a fight: King Krule or King K Rool?',
+        content: 'Blah blah blah blah blah blah blah blah blah blahblah blah blah blah blahblah blah blah blah blah blah blah blahblahblahblahblahblahblahblah blahblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+        tag: 'Shitpost',
+    },
+
+
+    {
+        username: 'doughboy420',
+        subject: 'I love $uicideBoy$',
+        content: 'Blah blah blah blah blah blah blah blah blah blahblah blah blah blah blahblah blah blah blah blah blah blah blahblahblahblahblahblahblahblah blahblah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+        tag: '$B',
+    },
+
+
+]
+
+export default blogs
---
+++ backend/seeder.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import blogs from './data/blogs.js'
+import User from './models/userModel.js';
+import Post from './models/postModel.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+ 
+    await User.deleteMany();
+    await Post.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+    
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleBlogs = blogs.map((post) => {
+      const submitter = createdUsers.find((user) => user.username === post.username)
+      if (submitter) {
+        return {...post, user:submitter._id };
+      }
+      else {
+        return {...post, user:null };
+      }
+    });
+
+    await Post.insertMany(sampleBlogs);
+
+    console.log('Data Imported!'.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    
+    await User.deleteMany();
+    await Post.deleteMany();
+
+    console.log('Data Destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
