2715f35efc9e7b7fe89cefec9f563d66ddfb8920
AditiAnan <aditianandjiwala10@gmail.com>
2024-04-18 15:28:22 -0400

    generate token files and registering user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,35 +1,25 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
-
-    const authUser = asyncHandler(async (req, res) => {
-        const { email, password } = req.body;
-      
-        const user = await User.findOne({ email });
-      
-        if (user && (await user.matchPassword(password))) {
-            const token = jwt.sign({ id: user._id }, process.env.
-                JWT_SECRET, {
-                    expiresIn: '30d',
-                }); 
-
-         res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-          });   
-          res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-          });
-        } else {
-          res.status(401);
-          throw new Error('Invalid email or password');
-        }
+import generateToken from '../utils/generateToken.js';
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+  
+    const user = await User.findOne({ email });
+  
+    if (user && (await user.matchPassword(password))) {
+      generateToken(res, user._id);
+  
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
       });
+    } else {
+      res.status(401);
+      throw new Error('Invalid email or password');
+    }
+  });
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
@@ -63,8 +53,15 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
-  })
+    res.clearCookie('jwt', '', {
+        httpOnly: true,
+        expirers: new Date(0),
+        
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
+
+
+});
 
 
 const getUserProfile = asyncHandler(async (req, res) => {
