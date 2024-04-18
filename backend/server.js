53b1644323c8edc2031813f10e5a9fe04b9f5c53
krennster <krennster17@gmail.com>
2024-04-18 10:56:37 -0400

    krenns changes

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,14 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+app.get('/api/blogs', (req, res) => {
+    res.json(blogs);
+});
+
+app.get('/api/blogs/:id', (req, res) => {
+    const blog = blogs.find((p) => p._id === req.params.id)
+    res.json(blog);
+});
+
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
